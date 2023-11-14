789d1e810b83bdb0ac358b9c79892fde3c9380ff
einfachalf <alexander.flemming@outlook.de>
2023-11-14 13:27:24 +0100

    dddf

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
   swcMinify: true,
   env: {
     HUGGINGFACE_API_TOKEN: process.env.HUGGINGFACE_API_TOKEN,
-    REPLICATE_API_TOKEN: process.env.REPLICATE_API_TOKEN,
-    OPENAI_API_TOKEN: process.env.OPENAI_API_TOKEN
+
   }
 };
